d803dded20f76aefdb9ddd0d66979c013cb8a4fb
samSquanch1 <samsquanchone@gmail.com>
2022-06-12 20:45:46 +0100

    Refactoring done
    Refactoring is done, Map Generator is gone, it is now terrain generator which generates the level in play. The editor preview is done in the new Map preview object, but it is flat atm.

--- Assets/EXPERIMENTAL/scripts/TextureGenerator.cs
+++ Assets/EXPERIMENTAL/scripts/TextureGenerator.cs
@@ -4,31 +4,27 @@ using UnityEngine;
 
 public static class TextureGenerator 
 {
-    public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height)
-    {
-        Texture2D texture = new Texture2D(width, height);
-        texture.filterMode = FilterMode.Point;
-        texture.wrapMode = TextureWrapMode.Clamp;
-        texture.SetPixels(colourMap);
-        texture.Apply();
-        return texture;
-    }
+   public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height) {
+		Texture2D texture = new Texture2D (width, height);
+		texture.filterMode = FilterMode.Point;
+		texture.wrapMode = TextureWrapMode.Clamp;
+		texture.SetPixels (colourMap);
+		texture.Apply ();
+		return texture;
+	}
 
-    public static Texture2D TextureFromHeightMap(float[,] heightMap)
-    {
-        int width = heightMap.GetLength(0); // getting the 'x', or first, dimenson of the float array
-        int height = heightMap.GetLength(1); // getting the 'y',or second
-        Color[] colourMap = new Color[width * height]; // One dimensoional color array with size of area 
 
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                colourMap[y * width + x] = Color.Lerp(Color.black, Color.white, heightMap[x, y]); // setting each point in color map in relation to x,y of the noiseMap
+	public static Texture2D TextureFromHeightMap(HeightMap heightMap) {
+		int width = heightMap.values.GetLength (0);
+		int height = heightMap.values.GetLength (1);
 
-            }
-        }
+		Color[] colourMap = new Color[width * height];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				colourMap [y * width + x] = Color.Lerp (Color.black, Color.white, Mathf.InverseLerp(heightMap.minValue,heightMap.maxValue,heightMap.values [x, y]));
+			}
+		}
 
-        return TextureFromColourMap(colourMap, width, height);
-    }
+		return TextureFromColourMap (colourMap, width, height);
+	}
 }
